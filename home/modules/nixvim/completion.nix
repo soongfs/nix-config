04e9d89486c1ad520e94f676d1ab1e6a6134b04d
soongfs <soongfs04@gmail.com>
2026-03-16 20:09:19 +0800

    update

--- home/modules/nixvim/completion.nix
+++ home/modules/nixvim/completion.nix
@@ -9,6 +9,36 @@
       enable = true;
       autoEnableSources = true;
       settings = {
+        completion.completeopt = "menu,menuone,noinsert,noselect";
+        mapping = {
+          __raw = ''
+            cmp.mapping.preset.insert({
+              ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+              ['<C-f>'] = cmp.mapping.scroll_docs(4),
+              ['<C-Space>'] = cmp.mapping.complete(),
+              ['<C-e>'] = cmp.mapping.abort(),
+              ['<Tab>'] = cmp.mapping(function(fallback)
+                if cmp.visible() then
+                  cmp.confirm({ select = true })
+                elseif require('luasnip').expand_or_jumpable() then
+                  require('luasnip').expand_or_jump()
+                else
+                  fallback()
+                end
+              end, { 'i', 's' }),
+              ['<S-Tab>'] = cmp.mapping(function(fallback)
+                if cmp.visible() then
+                  cmp.select_prev_item()
+                elseif require('luasnip').jumpable(-1) then
+                  require('luasnip').jump(-1)
+                else
+                  fallback()
+                end
+              end, { 'i', 's' }),
+            })
+          '';
+        };
+        preselect = "cmp.PreselectMode.None";
         snippet.expand = "function(args) require('luasnip').lsp_expand(args.body) end";
         sources = [
           { name = "nvim_lsp"; }
