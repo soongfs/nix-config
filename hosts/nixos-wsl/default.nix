872afb18bbb1fbdfc5072ce278afb604a9397239
soongfs <soongfs04@gmail.com>
2026-03-19 23:32:55 +0800

    update

--- hosts/nixos-wsl/default.nix
+++ hosts/nixos-wsl/default.nix
@@ -1,9 +1,11 @@
-{ ... }:
+{ username, ... }:
 
 {
   imports = [ ../../modules/nixos/presets/wsl.nix ];
 
   programs.fish.enable = true;
+
+  home-manager.users.${username}.systemd.user.startServices = "suggest";
 
   system.stateVersion = "25.05";
 }
